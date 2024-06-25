25d4d27486cf05d7f42646076c399c0c2888f71e
rayen kacem <kacemrayen13@gmail.com>
2024-06-25 22:57:33 +0100

    Initial Commit

---
+++ TrendingMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendingMoviesApp
+//
+//  Created by Mac Cooperation on 25/6/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
